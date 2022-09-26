c83b86b1e8bdc52b3c7778a87e65a6501f50c3aa
Jan Mitka <mitka.honza@gmail.com>
2022-09-26 14:53:51 +0200

    ShatteredGlass implementation started

--- data/rtscenes/TwoSpheres.cs
+++ data/rtscenes/TwoSpheres.cs
@@ -5,8 +5,8 @@ Debug.Assert(scene != null);
 Debug.Assert(context != null);
 
 // Override image resolution and supersampling.
-context[PropertyName.CTX_WIDTH]         = 640; // 1800, 1920
-context[PropertyName.CTX_HEIGHT]        = 480; // 1200, 1080
+context[PropertyName.CTX_WIDTH] = 640; // 1800, 1920
+context[PropertyName.CTX_HEIGHT] = 480; // 1200, 1080
 //context[PropertyName.CTX_SUPERSAMPLING] =   4; //  400,   64
 
 // Tooltip (if script uses values from 'param').
@@ -25,18 +25,18 @@ Util.TryParse(p, "n", ref n);
 
 // mat = {mirror|glass|diffuse}
 PhongMaterial pm = new PhongMaterial(new double[] {0.0, 0.2, 0.1}, 0.05, 0.05, 0.1, 128, sch);
-pm.n  = n;
+pm.n = n;
 pm.Kt = 0.9;
 
 if (p.TryGetValue("mat", out string mat))
   switch (mat)
   {
     case "diffuse":
-      pm = new PhongMaterial(new double[] {0.1, 0.1, 0.6}, 0.1, 0.8, 0.2, 16, sch);
+      pm = new PhongMaterial(new double[] { 0.1, 0.1, 0.6 }, 0.1, 0.8, 0.2, 16, sch);
       break;
 
     case "mirror":
-      pm = new PhongMaterial(new double[] {1.0, 1.0, 0.8}, 0.01, 0.0, 1.1, 8192, sch);
+      pm = new PhongMaterial(new double[] { 1.0, 1.0, 0.8 }, 0.01, 0.0, 1.1, 8192, sch);
       break;
 
     default:
@@ -49,11 +49,11 @@ if (p.TryGetValue("mat", out string mat))
 
 CSGInnerNode root = new CSGInnerNode(SetOperation.Union);
 root.SetAttribute(PropertyName.REFLECTANCE_MODEL, new PhongModel());
-root.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] {1.0, 0.7, 0.1}, 0.1, 0.7, 0.3, 128, sch));
+root.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] { 1.0, 0.7, 0.1 }, 0.1, 0.7, 0.3, 128, sch));
 scene.Intersectable = root;
 
 // Background color.
-scene.BackgroundColor = new double[] {0.0, 0.01, 0.03};
+scene.BackgroundColor = new double[] { 0.0, 0.01, 0.03 };
 scene.Background = new DefaultBackground(scene.BackgroundColor);
 
 // Camera.
@@ -80,6 +80,6 @@ root.InsertChild(s, Matrix4d.Scale(1.2) * Matrix4d.CreateTranslation(1.5, 0.2, 2
 
 // Infinite plane with checker.
 Plane pl = new Plane();
-pl.SetAttribute(PropertyName.COLOR, new double[] {0.3, 0.0, 0.0});
-pl.SetAttribute(PropertyName.TEXTURE, new CheckerTexture(0.6, 0.6, new double[] {1.0, 1.0, 1.0}));
+pl.SetAttribute(PropertyName.COLOR, new double[] { 0.3, 0.0, 0.0 });
+pl.SetAttribute(PropertyName.TEXTURE, new CheckerTexture(0.6, 0.6, new double[] { 1.0, 1.0, 1.0 }));
 root.InsertChild(pl, Matrix4d.RotateX(-MathHelper.PiOver2) * Matrix4d.CreateTranslation(0.0, -1.0, 0.0));
